32ee0f1580f855bb37559beda8a72434c20664ad
juanRCoder <guillecarck@gmail.com>
2023-08-07 16:09:48 -0700

    Test: FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,12 @@ import os
 import json
 import unittest
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
@@ -16,6 +22,12 @@ class TestFileStorage(unittest.TestCase):
     def test_file_object_is_list(self):
         file_object = FileStorage.__file_object
         self.assertIsInstance(file_object, dict, "file_object is not dict")
+
+    def test_all_method(self):
+        obj = FileStorage()
+        all_obj = obj.all()
+
+        self.assertIsInstance(all_obj, dict)
 
 
 if _name_ == '_main_':
