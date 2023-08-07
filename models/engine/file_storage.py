5e2403ef451833c8ef2ad50fc7a9f0c6ba111915
juanRCoder <guillecarck@gmail.com>
2023-08-07 07:31:01 -0700

    File: FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ from models.state import State
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.reviwe import Review
+from models.review import Review
 
 
 class FileStorage:
