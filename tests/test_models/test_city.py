a92e1abb05c0273c7c8faa8d42b126b91fac4031
juanRCoder <guillecarck@gmail.com>
2023-08-07 16:57:07 -0700

    Test: class City

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,13 +4,12 @@ from models.city import City
 
 
 class TestCity(unittest.TestCase):
-
-    def test_city_name_is_none(self):
-        city = City()
+    def Test_city_name_is_none(self):
+        city = City(name=None)
         self.assertIsNone(city.name, "name is not None")
 
     def test_city_state_id_is_none(self):
-        city = City()
+        city = City(state_id=None)
         self.assertIsNone(city.state_id, "State_id is not None")
 
 
